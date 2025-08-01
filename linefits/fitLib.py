a405f58e042c20ce2eadf397c8514a27e531849c
Thomaslund1 <159867737+Thomaslund1@users.noreply.github.com>
2025-08-01 16:35:25 -0500

    Update fitLib.py
    fixing comments

--- linefits/fitLib.py
+++ linefits/fitLib.py
@@ -14,6 +14,8 @@ import datetime
 import astropy
 import astropy.time
 from packaging import version
+import pickle
+import matplotlib.pyplot as plt
 try: #Packages needed for LSF 
     from NeidLsf import *
     import pickle
